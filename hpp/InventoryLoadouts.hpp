49b9caa22b94b2ff2044e821d2c4ea6bbecb97b7
NikLoc <38963934+NikLoc@users.noreply.github.com>
2018-05-04 16:51:14 +0200

    CfgRespawnInventory hpp

---
+++ hpp/InventoryLoadouts.hpp
@@ -0,0 +1,11 @@
+class CfgRespawnInventory
+{
+		class CommanderB
+		{
+			weapons[] = {"arifle_MXC_F"};
+			magazines[] = {"30Rnd_65x39_caseless_mag","30Rnd_65x39_caseless_mag","30Rnd_65x39_caseless_mag","30Rnd_65x39_caseless_mag","HandGrenade","SmokeShellBlue","SmokeShell"};
+			items[] = {"ACE_MapTools","ACE_EarPlugs","ACE_microDAGR","ACE_fieldDressing","ACE_fieldDressing","ACE_morphine","ACE_fieldDressing","ACE_fieldDressing","ACE_morphine"};
+			linkedItems[] = {"V_PlateCarrier1_rgr","tf_rt1523g","H_Beret_Colonel","G_Aviator","ACE_optic_Hamr_PIP","ItemMap","ItemCompass","ItemWatch","tf_anprc152_1","NVGoggles"};
+			uniformclass[] = {"U_B_CombatUniform_mcam"};
+		};
+};

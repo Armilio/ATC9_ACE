9a4023c22920e0a2431463efce14663ed190da2d
NikLoc <38963934+NikLoc@users.noreply.github.com>
2018-05-07 02:43:44 +0200

    Aggiunti livelli comodi
    Il sistema lavora su classe "Commander", side "B" e livello "3". Siccome non ho voglia di fare un sistema che escluda in casi particolari la terza farò dei livelli fantoccio tra i quali non cambierà una benemerita cippa.

--- hpp/InventoryLoadouts.hpp
+++ hpp/InventoryLoadouts.hpp
@@ -1,6 +1,16 @@
 class CfgRespawnInventory
 {
-		class CommanderB
+		class CommanderB3
+		{
+			weapons[] = {"arifle_MXC_F","Binocular"};
+			magazines[] = {"30Rnd_65x39_caseless_mag","30Rnd_65x39_caseless_mag","30Rnd_65x39_caseless_mag","30Rnd_65x39_caseless_mag","HandGrenade","SmokeShellBlue","SmokeShell"};
+			items[] = {"ACE_MapTools","ACE_EarPlugs","ACE_microDAGR","FirstAidKit","FirstAidKit"};
+			linkedItems[] = {"V_PlateCarrier1_rgr","tf_rt1523g","H_Beret_Colonel","G_Aviator","ACE_optic_Hamr_PIP","ItemMap","ItemCompass","ItemWatch","tf_anprc152_1","NVGoggles"};
+			uniformclass = "U_B_CombatUniform_mcam";
+			backpack = "tf_rt1523g";
+		};
+		
+		class CommanderB2
 		{
 			weapons[] = {"arifle_MXC_F","Binocular"};
 			magazines[] = {"30Rnd_65x39_caseless_mag","30Rnd_65x39_caseless_mag","30Rnd_65x39_caseless_mag","30Rnd_65x39_caseless_mag","HandGrenade","SmokeShellBlue","SmokeShell"};
@@ -10,7 +20,17 @@ class CfgRespawnInventory
 			backpack = "tf_rt1523g";
 		};
 	
-		class CommanderR
+		class CommanderB1
+		{
+			weapons[] = {"arifle_MXC_F","Binocular"};
+			magazines[] = {"30Rnd_65x39_caseless_mag","30Rnd_65x39_caseless_mag","30Rnd_65x39_caseless_mag","30Rnd_65x39_caseless_mag","HandGrenade","SmokeShellBlue","SmokeShell"};
+			items[] = {"ACE_MapTools","ACE_EarPlugs","ACE_microDAGR","FirstAidKit","FirstAidKit"};
+			linkedItems[] = {"V_PlateCarrier1_rgr","tf_rt1523g","H_Beret_Colonel","G_Aviator","ACE_optic_Hamr_PIP","ItemMap","ItemCompass","ItemWatch","tf_anprc152_1","NVGoggles"};
+			uniformclass = "U_B_CombatUniform_mcam";
+			backpack = "tf_rt1523g";
+		};
+	
+		class CommanderR3
 		{
 			weapons[] = {"arifle_Katiba_F","Binocular"};
 			magazines[] = {"30Rnd_65x39_caseless_green","30Rnd_65x39_caseless_green","30Rnd_65x39_caseless_green","30Rnd_65x39_caseless_green","30Rnd_65x39_caseless_green","30Rnd_65x39_caseless_green","handgrenade","smokeshellred","SmokeShell"};
@@ -20,7 +40,27 @@ class CfgRespawnInventory
 			backpack = "tf_mr3000";
 		};
 	
-		class TeamLeaderB1
+		class CommanderR2
+		{
+			weapons[] = {"arifle_Katiba_F","Binocular"};
+			magazines[] = {"30Rnd_65x39_caseless_green","30Rnd_65x39_caseless_green","30Rnd_65x39_caseless_green","30Rnd_65x39_caseless_green","30Rnd_65x39_caseless_green","30Rnd_65x39_caseless_green","handgrenade","smokeshellred","SmokeShell"};
+			items[] = {"FirstAidKit","FirstAidKit","ACE_MapTools","ACE_EarPlugs","ACE_microDAGR"};
+			linkedItems[] = {"ItemMap","ItemCompass","ItemWatch","tf_fadak","NVGoggles_OPFOR","ACE_optic_Arco_PIP","V_HarnessO_brn","H_MilCap_ocamo"};
+			uniformClass = "U_O_OfficerUniform_ocamo";
+			backpack = "tf_mr3000";
+		};
+	
+		class CommanderR1
+		{
+			weapons[] = {"arifle_Katiba_F","Binocular"};
+			magazines[] = {"30Rnd_65x39_caseless_green","30Rnd_65x39_caseless_green","30Rnd_65x39_caseless_green","30Rnd_65x39_caseless_green","30Rnd_65x39_caseless_green","30Rnd_65x39_caseless_green","handgrenade","smokeshellred","SmokeShell"};
+			items[] = {"FirstAidKit","FirstAidKit","ACE_MapTools","ACE_EarPlugs","ACE_microDAGR"};
+			linkedItems[] = {"ItemMap","ItemCompass","ItemWatch","tf_fadak","NVGoggles_OPFOR","ACE_optic_Arco_PIP","V_HarnessO_brn","H_MilCap_ocamo"};
+			uniformClass = "U_O_OfficerUniform_ocamo";
+			backpack = "tf_mr3000";
+		};
+	
+		class TeamLeaderB3
 		{
 			weapons[] = {"arifle_MX_GL_F","hgun_P07_F","Binocular"};
 			magazines[] = {"16Rnd_9x21_Mag","16Rnd_9x21_Mag","30Rnd_65x39_caseless_mag","30Rnd_65x39_caseless_mag","30Rnd_65x39_caseless_mag","30Rnd_65x39_caseless_mag","30Rnd_65x39_caseless_mag","30Rnd_65x39_caseless_mag","handgrenade","1Rnd_HE_Grenade_shell","1Rnd_HE_Grenade_shell","1Rnd_HE_Grenade_shell","1Rnd_HE_Grenade_shell","smokeshellblue","SmokeShell"};
@@ -40,7 +80,7 @@ class CfgRespawnInventory
 			backpack = "tf_rt1523g";
 		};
 		
-		class TeamLeaderB3
+		class TeamLeaderB1
 		{
 			weapons[] = {"arifle_MX_F","hgun_P07_F","Binocular"};
 			magazines[] = {"16Rnd_9x21_Mag","16Rnd_9x21_Mag","30Rnd_65x39_caseless_mag","30Rnd_65x39_caseless_mag","30Rnd_65x39_caseless_mag","30Rnd_65x39_caseless_mag","30Rnd_65x39_caseless_mag","30Rnd_65x39_caseless_mag","handgrenade","smokeshellblue","SmokeShell"};
@@ -50,7 +90,7 @@ class CfgRespawnInventory
 			backpack = "tf_rt1523g";
 		};
 	
-		class TeamLeaderR1
+		class TeamLeaderR3
 		{
 			weapons[] = {"arifle_Katiba_GL_F","hgun_Rook40_F","Binocular"};
 			magazines[] = {"16Rnd_9x21_Mag","16Rnd_9x21_Mag","30Rnd_65x39_caseless_green","30Rnd_65x39_caseless_green","30Rnd_65x39_caseless_green","30Rnd_65x39_caseless_green","30Rnd_65x39_caseless_green","30Rnd_65x39_caseless_green","handgrenade","1Rnd_HE_Grenade_shell","1Rnd_HE_Grenade_shell","1Rnd_HE_Grenade_shell","1Rnd_HE_Grenade_shell","smokeshellblue","SmokeShell"};
@@ -70,7 +110,7 @@ class CfgRespawnInventory
 			backpack = "tf_rt1523g";
 		};
 	
-		class TeamLeaderR3
+		class TeamLeaderR1
 		{
 			weapons[] = {"arifle_Katiba_F","hgun_Rook40_F","Binocular"};
 			magazines[] = {"16Rnd_9x21_Mag","16Rnd_9x21_Mag","30Rnd_65x39_caseless_green","30Rnd_65x39_caseless_green","30Rnd_65x39_caseless_green","30Rnd_65x39_caseless_green","30Rnd_65x39_caseless_green","30Rnd_65x39_caseless_green","handgrenade","smokeshellblue","SmokeShell"};
